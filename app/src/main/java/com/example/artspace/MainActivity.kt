2bee94ad9550bb72ebb09999ca5c82855983c29a
kep_ildar <i.davletyarov@gmail.com>
2024-11-06 15:50:20 +0300

    Added files and basics

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,8 +3,21 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.artspace.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +27,56 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
+                    ArtSpaceScreen()
                 }
             }
+        }
+    }
+
+    data class Artwork(
+        val title: String,
+        val artist: String,
+        val year: Int,
+        @DrawableRes val image: Int
+    )
+
+    object Data {
+        val images = listOf(
+            Artwork(
+                title = "Название 1",
+                artist = "Автор 1",
+                year = 2024,
+                image = R.drawable.art1
+            ),
+            Artwork(
+                title = "Название 1",
+                artist = "Автор 1",
+                year = 2023,
+                image = R.drawable.art2
+            ),
+            Artwork(
+                title = "Название 1",
+                artist = "Автор 1",
+                year = 2022,
+                image = R.drawable.art3
+            )
+        )
+    }
+
+    @Composable
+    fun ArtSpaceScreen() {
+        val artworkList = Data.images
+        var currentArtworkIndex by remember { mutableStateOf(0) }
+        val currentArtwork = artworkList[currentArtworkIndex]
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(text = currentArtwork.title, fontSize = 20.sp)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = "by ${currentArtwork.artist}", fontSize = 16.sp)
         }
     }
 }
